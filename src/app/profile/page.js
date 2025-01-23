f8d31adf2fa488cd7b6f9963a581dc73db1ee655
kobirul5 <kobirul05j@gmail.com>
2025-01-23 15:40:43 +0600

    fixed bugs

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,18 +1,22 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";import Link from 'next/link';
-import React from 'react';
+import { getKindeServerSession, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from 'next/link';
+import React, { use } from 'react';
+import { redirect } from "next/navigation";
+
+
 
 
 const Profile = async () => {
     const {getUser} = getKindeServerSession();
     const user = await getUser();
-   
-    if(!user){
-
-    }
+   if(!user) {
+    redirect('/api/auth/login?post_login_redirect_url=/dashboard')
+   }
+  
 
     return (
         <div>
-            profile
+            Welcome to your profile!
         </div>
     );
 };
